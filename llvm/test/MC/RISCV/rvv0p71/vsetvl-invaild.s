00a6b0430bf9780ac4900d5cab7651ffa9e2368c
imkiva <imkiva@islovely.icu>
2023-08-08 16:27:40 +0800

    [RVV 0.7.1] rework error message
    Related upstream change: https://reviews.llvm.org/D150753

--- llvm/test/MC/RISCV/rvv0p71/vsetvl-invaild.s
+++ llvm/test/MC/RISCV/rvv0p71/vsetvl-invaild.s
@@ -2,7 +2,7 @@
 # RUN: not llvm-mc -triple=riscv64 -show-encoding --mattr=+xtheadv %s 2>&1 | FileCheck %s
 
 vsetvli a2, a1, e8
-# CHECK: operand must be e[8|16|32|64|128|256|512|1024],m[1|2|4|8],d[1|2|4|8] for RVV0.71
+# CHECK: error: operand must be e[8|16|32|64|128|256|512|1024],m[1|2|4|8|f2|f4|f8],[ta|tu],[ma|mu]
 
 vsetivli a2, 15, 208
 # CHECK: instruction requires the following: 'V' (Vector Extension for Application Processors), 'Zve32x' or 'Zve64x' (Vector Extensions for Embedded Processors){{$}}
@@ -11,5 +11,5 @@ vsetivli a2, 0, e32, m1, ta, ma
 # CHECK: instruction requires the following: 'V' (Vector Extension for Application Processors), 'Zve32x' or 'Zve64x' (Vector Extensions for Embedded Processors){{$}}
 
 vsetivli a2, 0, e32, m1, d1
-# CHECK: invalid operand for instruction
+# CHECK: error: operand must be e[8|16|32|64|128|256|512|1024],m[1|2|4|8|f2|f4|f8],[ta|tu],[ma|mu]
 
