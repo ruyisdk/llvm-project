98eb36f1e5ac088fa9f3db1901df74d22ec12c8a
yanming <ming.yan@terapines.com>
2023-08-08 14:48:12 +0800

    [RVV 0.7.1] cherry-pick from Terapines/llvm-project (MC tests)

---
+++ llvm/test/MC/RISCV/rvv0p71/vsetvl-invaild.s
@@ -0,0 +1,15 @@
+# RUN: not llvm-mc -triple=riscv32 -show-encoding --mattr=+xv %s 2>&1 | FileCheck %s
+# RUN: not llvm-mc -triple=riscv64 -show-encoding --mattr=+xv %s 2>&1 | FileCheck %s
+
+vsetvli a2, a1, e8
+# CHECK: operand must be e[8|16|32|64|128|256|512|1024],m[1|2|4|8],d[1|2|4|8] for RVV0.71
+
+vsetivli a2, 15, 208
+# CHECK: instruction requires the following: 'V' (Vector Extension for Application Processors), 'Zve32x' or 'Zve64x' (Vector Extensions for Embedded Processors){{$}}
+
+vsetivli a2, 0, e32, m1, ta, ma
+# CHECK: instruction requires the following: 'V' (Vector Extension for Application Processors), 'Zve32x' or 'Zve64x' (Vector Extensions for Embedded Processors){{$}}
+
+vsetivli a2, 0, e32, m1, d1
+# CHECK: invalid operand for instruction
+
