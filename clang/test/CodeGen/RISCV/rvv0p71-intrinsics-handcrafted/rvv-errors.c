5f842003f50ce746a50a30a41649c9c738cd4e68
imkiva <imkiva@islovely.icu>
2023-10-31 15:26:38 +0800

    [Clang][RVV 0.7.1] Initial source-level compatibility with RVV 1.0

--- clang/test/CodeGen/RISCV/rvv0p71-intrinsics-handcrafted/rvv-errors.c
+++ clang/test/CodeGen/RISCV/rvv0p71-intrinsics-handcrafted/rvv-errors.c
@@ -1,10 +1,12 @@
 // RUN: %clang_cc1 %s -triple=riscv64 -target-feature +xtheadv -fsyntax-only -verify
 
+#include <riscv_vector_xtheadv.h>
+
 void test(void) {
-  __builtin_rvv_xvsetvl(1, 7, 0); // expected-error {{argument value 7 is outside the valid range [0, 3]}}
-  __builtin_rvv_xvsetvlmax(-1, 0); // expected-error {{argument value 18446744073709551615 is outside the valid range [0, 3]}}
-  __builtin_rvv_xvsetvl(1, 0, 4); // expected-error {{LMUL argument must be in the range [0,3]}}
-  __builtin_rvv_xvsetvlmax(0, 4); // expected-error {{LMUL argument must be in the range [0,3]}}
-  __builtin_rvv_xvsetvl(1, 0, 8); // expected-error {{LMUL argument must be in the range [0,3]}}
-  __builtin_rvv_xvsetvlmax(0, -1); // expected-error {{LMUL argument must be in the range [0,3]}}
+  __builtin_rvv_vsetvli(1, 7, 0); // expected-error {{argument value 7 is outside the valid range [0, 3]}}
+  __builtin_rvv_vsetvlimax(-1, 0); // expected-error {{argument value 18446744073709551615 is outside the valid range [0, 3]}}
+  __builtin_rvv_vsetvli(1, 0, 4); // expected-error {{LMUL argument must be in the range [0,3]}}
+  __builtin_rvv_vsetvlimax(0, 4); // expected-error {{LMUL argument must be in the range [0,3]}}
+  __builtin_rvv_vsetvli(1, 0, 8); // expected-error {{LMUL argument must be in the range [0,3]}}
+  __builtin_rvv_vsetvlimax(0, -1); // expected-error {{LMUL argument must be in the range [0,3]}}
 }
