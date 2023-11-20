33b205281e10f98fb57a9f1f0a24a3b1bcf1954a
AinsleySnow <AinsleySnow@outlook.com>
2023-11-20 18:12:25 +0800

    [LLVM] [RVV 0.7.1] Enable vector with float-point element type in RVV 0.7.1

--- llvm/lib/Target/RISCV/RISCVSubtarget.h
+++ llvm/lib/Target/RISCV/RISCVSubtarget.h
@@ -172,10 +172,10 @@ public:
   bool hasVInstructionsI64() const {
     return hasOnlyStdVI64() || hasVendorXTHeadV();
   }
-  bool hasVInstructionsF16() const { return hasOnlyStdVF16(); }
-  bool hasVInstructionsF32() const { return hasOnlyStdVF32(); }
-  bool hasVInstructionsF64() const { return hasOnlyStdVF64(); }
-  bool hasVInstructionsAnyF() const { return hasOnlyStdVAnyF(); }
+  bool hasVInstructionsF16() const { return hasOnlyStdVF16() || hasVendorXTHeadV(); }
+  bool hasVInstructionsF32() const { return hasOnlyStdVF32() || hasVendorXTHeadV(); }
+  bool hasVInstructionsF64() const { return hasOnlyStdVF64() || hasVendorXTHeadV(); }
+  bool hasVInstructionsAnyF() const { return hasOnlyStdVAnyF() || hasVendorXTHeadV(); }
   bool hasVInstructionsFullMultiply() const { return hasOnlyStdV() || hasVendorXTHeadV(); }
   //  If a SubTarget only has the standard V extension:
   bool hasOnlyStdV() const {
