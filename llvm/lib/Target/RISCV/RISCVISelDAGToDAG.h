d0df6e3430aa00c5335640a04cb8c52491acde6d
AinsleySnow <772571228@qq.com>
2023-12-08 16:01:18 +0800

    [LLVM][XTHeadVector] Implement intrinsic for unit-stride segment loads and stores. (#37)
    * [LLVM][RVV 0.7.1] Rename elements in search
    table to match T-Head suggested naming scheme.
    
    * [LLVM][XTHeadVector] Define intrinsic functions
    for unit-strided segment load.
    
    * [LLVM][XTHeadVector] Define search table for vlseg
    intrinsic.
    
    * [LLVM][XTHeadVector] Define pseudo nodes for vlseg
    intrinsic.
    
    * [LLVM][XTHeadVector] Expand vlseg load pseudo
    nodes.
    
    * [LLVM][XTHeadVector] Set load/store info for newly
    added intrinsic.
    
    * [LLVM][XTHeadVector] Add tests for new intrinsic.
    
    * [LLVM][XTHeadVector] Define intrinsic for segment
    store.
    
    * [LLVM][XTHeadVector] Define search table for seg
    store intrinsic.
    
    * [LLVM][XTHeadVector] Define pseudo nodes for
    segment store intrinsic.
    
    * [LLVM][XTHeadVector] Select segment store pseudos.
    
    * [LLVM][XTHeadVector] Set load and store info for
    segment store intrinsic.
    
    * [LLVM][XTHeadVector] Add tests.

--- llvm/lib/Target/RISCV/RISCVISelDAGToDAG.h
+++ llvm/lib/Target/RISCV/RISCVISelDAGToDAG.h
@@ -160,6 +160,8 @@ public:
                  bool IsMasked, bool IsStrided, bool IsIndexed, bool IsFF, bool IsE);
   void selectXVS(SDNode *Node, const SDLoc& DL, unsigned IntNo,
                  bool IsMasked, bool IsStrided, bool IsIndexed, bool IsE);
+  void selectXVLSEG(SDNode *Node, unsigned IntNo, bool IsMasked, bool IsUnsigned, bool IsE);
+  void selectXVSSEG(SDNode *Node, unsigned IntNo, bool IsMasked, bool IsE);
   void selectVLSEG(SDNode *Node, bool IsMasked, bool IsStrided);
   void selectVLSEGFF(SDNode *Node, bool IsMasked);
   void selectVLXSEG(SDNode *Node, bool IsMasked, bool IsOrdered);
@@ -241,6 +243,16 @@ struct VSSEGPseudo {
   uint16_t Pseudo;
 };
 
+struct TH_VSSEGPseudo {
+  uint16_t NF : 4;
+  uint16_t Masked : 1;
+  uint16_t IsE : 1;
+  uint16_t Log2MEM : 3;
+  uint16_t Log2SEW : 3;
+  uint16_t LMUL : 3;
+  uint16_t Pseudo;
+};
+
 struct VSXSEGPseudo {
   uint16_t NF : 4;
   uint16_t Masked : 1;
@@ -248,6 +260,17 @@ struct VSXSEGPseudo {
   uint16_t Log2SEW : 3;
   uint16_t LMUL : 3;
   uint16_t IndexLMUL : 3;
+  uint16_t Pseudo;
+};
+
+struct TH_VLSEGPseudo {
+  uint16_t NF : 4;
+  uint16_t Masked : 1;
+  uint16_t Unsigned : 1;
+  uint16_t IsE : 1;
+  uint16_t Log2MEM : 3;
+  uint16_t Log2SEW : 3;
+  uint16_t LMUL : 3;
   uint16_t Pseudo;
 };
 
@@ -260,7 +283,7 @@ struct VLEPseudo {
   uint16_t Pseudo;
 };
 
-struct XVLPseudo {
+struct TH_VLPseudo {
   uint16_t Masked : 1;
   uint16_t Strided : 1;
   uint16_t Indexed : 1;
@@ -281,7 +304,7 @@ struct VSEPseudo {
   uint16_t Pseudo;
 };
 
-struct XVSPseudo {
+struct TH_VSPseudo {
   uint16_t Masked : 1;
   uint16_t Strided : 1;
   uint16_t Indexed : 1;
@@ -311,6 +334,8 @@ struct VLX_VSXPseudo {
 #define GET_RISCVVSXTable_DECL
 #define GET_XTHeadVVLTable_DECL
 #define GET_XTHeadVVSTable_DECL
+#define GET_XTHeadVVLSEGTable_DECL
+#define GET_XTHeadVVSSEGTable_DECL
 } // namespace RISCV
 
 } // namespace llvm
