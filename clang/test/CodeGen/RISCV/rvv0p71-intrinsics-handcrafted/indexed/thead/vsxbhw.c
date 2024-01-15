b4c6607e241e174ce1406be18d0733facd4a0a24
imkiva <imkiva@islovely.icu>
2024-01-15 16:25:23 +0800

    [Clang][XTHeadVector] Add generate tests

---
+++ clang/test/CodeGen/RISCV/rvv0p71-intrinsics-handcrafted/indexed/thead/vsxbhw.c
@@ -0,0 +1,16 @@
+// RUN: %clang_cc1 -triple riscv64 -target-feature +xtheadvector \
+// RUN:   -disable-O0-optnone -emit-llvm %s -o - | \
+// RUN:   opt -S -passes=mem2reg | \
+// RUN:   FileCheck --check-prefix=CHECK-RV64 %s
+
+#include <riscv_vector.h>
+
+// CHECK-RV64-LABEL: define dso_local void @test_th_vsxb_v_i8m1
+// CHECK-RV64-SAME: (ptr noundef [[BASE:%.*]], <vscale x 8 x i8> [[index:%.*]], <vscale x 8 x i8> [[VALUE:%.*]], i64 noundef [[VL:%.*]]) #[[ATTR0:[0-9]+]] {
+// CHECK-RV64-NEXT:  entry:
+// CHECK-RV64-NEXT:    call void @llvm.riscv.th.vsxb.nxv8i8.nxv8i8.i64(<vscale x 8 x i8> [[VALUE]], ptr [[BASE]], <vscale x 8 x i8> [[index]], i64 [[VL]])
+// CHECK-RV64-NEXT:    ret void
+//
+void test_th_vsxb_v_i8m1(int8_t *base, vuint8m1_t index, vint8m1_t value, size_t vl) {
+  return __riscv_th_vsxb_v_i8m1(base, index, value, vl);
+}
