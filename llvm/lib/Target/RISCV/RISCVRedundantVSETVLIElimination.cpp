b7432dce55c1b45ffbb7117adc9e0b3980b54e93
imkiva <imkiva@islovely.icu>
2024-04-10 22:41:11 +0800

    [LLVM][XTHeadVector] `RedundantVSETVLIElimination`
    This pass removes code sequences like:
    
    ```
    csrr <r1>, vl
    csrr <r2>, vtype
    <a lot of th.vsetvl/th.vsetvli>
    th.vsetvl zero, <r1>, <r2>
    ```

--- llvm/lib/Target/RISCV/RISCVRedundantVSETVLIElimination.cpp
+++ llvm/lib/Target/RISCV/RISCVRedundantVSETVLIElimination.cpp
@@ -58,10 +58,103 @@ bool RISCVRedundantVSETVLIElimination::optimizeBlock(MachineBasicBlock &MBB) {
   // ; CHECK-NEXT:    csrr a2, vtype
   // ; CHECK-NEXT:    <a lot of th.vsetvl/vsetvli>
   // ; CHECK-NEXT:    th.vsetvl zero, a1, a2
-  return false;
+
+  enum State {
+    OTHER,
+    BACKUP_VL,
+    BACKUP_VTYPE,
+    SEQUENCE,
+  };
+
+  State NowState = OTHER;
+  MachineBasicBlock::iterator FoundBegin = MBB.end();
+  Register SavedVL = RISCV::NoRegister;
+  Register SavedVTYPE = RISCV::NoRegister;
+
+  const auto VLEncoding = RISCVSysReg::lookupSysRegByName("VL")->Encoding;
+  const auto VTYPEEncoding = RISCVSysReg::lookupSysRegByName("VTYPE")->Encoding;
+
+  bool Changed = false;
+
+  // Remove redundant Copy instructions unless TargetReg is modified.
+  for (MachineBasicBlock::iterator I = MBB.begin(), E = MBB.end(); I != E;) {
+    MachineInstr *MI = &*I;
+    ++I;
+
+    // CSRRS saved_vl, VL, x0
+    if (NowState == OTHER) {
+      if (MI->getOpcode() == RISCV::CSRRS) {
+        if (MI->getOperand(1).getImm() == VLEncoding) {
+          NowState = BACKUP_VL;
+          SavedVL = MI->getOperand(0).getReg();
+          FoundBegin = MI;
+          continue;
+        }
+        NowState = OTHER;
+      }
+    }
+
+    // CSRRS saved_vtype, VTYPE, x0
+    if (NowState == BACKUP_VL) {
+      if (MI->getOpcode() == RISCV::CSRRS) {
+        if (MI->getOperand(1).getImm() == VTYPEEncoding) {
+          NowState = BACKUP_VTYPE;
+          SavedVTYPE = MI->getOperand(0).getReg();
+          continue;
+        }
+        NowState = OTHER;
+      }
+    }
+
+    if (NowState == BACKUP_VTYPE) {
+      if (MI->getOpcode() == RISCV::TH_VSETVL ||
+          MI->getOpcode() == RISCV::TH_VSETVLI ||
+          MI->getOpcode() == RISCV::PseudoTH_VSETVLI ||
+          MI->getOpcode() == RISCV::PseudoTH_VSETVLIX0) {
+        NowState = SEQUENCE;
+        continue;
+      }
+      NowState = OTHER;
+    }
+
+    if (NowState == SEQUENCE) {
+      if (MI->getOpcode() == RISCV::TH_VSETVLI ||
+          MI->getOpcode() == RISCV::PseudoTH_VSETVLI ||
+          MI->getOpcode() == RISCV::PseudoTH_VSETVLIX0) {
+        continue;
+      }
+
+      if (MI->getOpcode() == RISCV::TH_VSETVL) {
+        // Check if this is a restore point
+        if (MI->getOperand(0).getReg() == RISCV::X0 &&
+            MI->getOperand(1).getReg() == SavedVL &&
+            MI->getOperand(2).getReg() == SavedVTYPE) {
+          assert(FoundBegin != MBB.end() &&
+                 "FoundBegin is not set but restore point is found");
+          // Now erase the sequence from the FoundBegin to MI
+          while (FoundBegin != MI) {
+            MachineInstr *ToErase = &*FoundBegin;
+            ++FoundBegin;
+            ToErase->eraseFromParent();
+            ++NumVSETVLIRemoved;
+            Changed = true;
+          }
+          MI->eraseFromParent();
+          ++NumVSETVLIRemoved;
+          NowState = OTHER;
+        }
+        continue;
+      }
+
+      NowState = OTHER;
+    }
+  }
+
+  return Changed;
 }
 
-bool RISCVRedundantVSETVLIElimination::runOnMachineFunction(MachineFunction &MF) {
+bool RISCVRedundantVSETVLIElimination::runOnMachineFunction(
+    MachineFunction &MF) {
   if (skipFunction(MF.getFunction()))
     return false;
 
